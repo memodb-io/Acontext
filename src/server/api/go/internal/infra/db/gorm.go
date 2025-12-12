37323d994b6443e2c8adb10c275251dd7179cfec
Gener <435669237@qq.com>
2025-12-12 12:02:19 +0800

    feat(api): add TLS support for database, Redis, and RabbitMQ

--- src/server/api/go/internal/infra/db/gorm.go
+++ src/server/api/go/internal/infra/db/gorm.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/memodb-io/Acontext/internal/config"
@@ -14,7 +16,26 @@ func New(cfg *config.Config) (*gorm.DB, error) {
 	gcfg := &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Warn),
 	}
-	db, err := gorm.Open(postgres.Open(cfg.Database.DSN), gcfg)
+
+	// Adjust DSN sslmode based on EnableTLS configuration
+	dsn := cfg.Database.DSN
+	if cfg.Database.EnableTLS {
+		// Replace sslmode=disable with sslmode=require when TLS is enabled
+		// Use regex to handle various formats (sslmode=disable, sslmode=disable, etc.)
+		sslmodeRegex := regexp.MustCompile(`(?i)\bsslmode\s*=\s*\w+`)
+		if sslmodeRegex.MatchString(dsn) {
+			// Replace existing sslmode
+			dsn = sslmodeRegex.ReplaceAllString(dsn, "sslmode=require")
+		} else {
+			// Append sslmode if not present
+			if !strings.HasSuffix(dsn, " ") {
+				dsn += " "
+			}
+			dsn += "sslmode=require"
+		}
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), gcfg)
 	if err != nil {
 		return nil, err
 	}
