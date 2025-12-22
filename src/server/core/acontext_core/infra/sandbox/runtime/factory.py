b79c4dfb2795cb4da7e6bcaf0f11b440d601f2a4
mbt1909432 <1138932382@qq.com>
2025-12-22 11:09:59 +0800

    delete Backward-compatible

--- src/server/core/acontext_core/infra/sandbox/runtime/factory.py
+++
@@ -1,9 +0,0 @@
-from ..lifecycle.models import SandboxInfo
-from .base import SandboxRuntime
-from .registry import get_runtime_for_sandbox as _dispatch_runtime
-
-
-def get_runtime_for_sandbox(sandbox: SandboxInfo) -> SandboxRuntime:
-    """Backward-compatible wrapper to dispatch runtime by backend."""
-    return _dispatch_runtime(sandbox)
-
