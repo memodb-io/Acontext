19009f62dfaf6542c3e2a508959faa9245fe41d1
riturajFi <riturajtripathy10x@gmail.com>
2026-01-13 14:18:42 +0530

    test middle_out requires token_reduce_to

--- src/server/api/go/internal/pkg/editor/strategy_middle_out_test.go
+++ src/server/api/go/internal/pkg/editor/strategy_middle_out_test.go
@@ -2,8 +2,12 @@ package editor
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateMiddleOutStrategy(t *testing.T) {
-	_ = t
+	_, err := createMiddleOutStrategy(map[string]interface{}{})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "token_reduce_to")
 }
