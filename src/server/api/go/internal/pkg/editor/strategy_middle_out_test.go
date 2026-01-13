d301fd06c231db812b5b37652d15ea7cd8829615
riturajFi <riturajtripathy10x@gmail.com>
2026-01-13 14:18:55 +0530

    test middle_out rejects non-integer token_reduce_to

--- src/server/api/go/internal/pkg/editor/strategy_middle_out_test.go
+++ src/server/api/go/internal/pkg/editor/strategy_middle_out_test.go
@@ -10,4 +10,8 @@ func TestCreateMiddleOutStrategy(t *testing.T) {
 	_, err := createMiddleOutStrategy(map[string]interface{}{})
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "token_reduce_to")
+
+	_, err = createMiddleOutStrategy(map[string]interface{}{"token_reduce_to": "bad"})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "must be an integer")
 }
