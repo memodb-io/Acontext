700edbdb28f8900c73033bce42728bc4b6187f8a
riturajFi <riturajtripathy10x@gmail.com>
2026-01-13 14:08:18 +0530

    validate middle_out token_reduce_to presence

--- src/server/api/go/internal/pkg/editor/strategy_middle_out.go
+++ src/server/api/go/internal/pkg/editor/strategy_middle_out.go
@@ -7,5 +7,8 @@ type MiddleOutStrategy struct{ TokenReduceTo int }
 func (s *MiddleOutStrategy) Name() string { return "middle_out" }
 
 func createMiddleOutStrategy(params map[string]interface{}) (EditStrategy, error) {
+	if _, ok := params["token_reduce_to"]; !ok {
+		return nil, fmt.Errorf("middle_out strategy requires 'token_reduce_to' parameter")
+	}
 	return nil, fmt.Errorf("middle_out strategy not implemented")
 }
