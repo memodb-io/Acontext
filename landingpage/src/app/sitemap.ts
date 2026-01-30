bfeadc6e5d4df8a10ea6a8ea46c33b5d3f6768ce
Gener <435669237@qq.com>
2026-01-30 20:25:57 +0800

    feat(landingpage): add new product sandbox URL to sitemap

--- landingpage/src/app/sitemap.ts
+++ landingpage/src/app/sitemap.ts
@@ -27,6 +27,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/product/sandbox`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: 'daily',
